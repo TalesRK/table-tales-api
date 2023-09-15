18ff7c699c4f5f1ea76473914d56e10468b9a206
tales.kerschner <tales.kerschner@cwi.com.br>
2023-09-15 14:53:15 -0300

    initial commit

---
+++ src/test/java/com/tabletales/TableTalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabletales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
